640dd8354877fe343cbdee66b8c523616c076a99
AgoSorrentino <ago.sorrentino@gmail.com>
2025-06-20 10:22:10 +0200

    init commit

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,72 @@
+
+package controller;
+
+import modello.*;
+import java.util.List;
+
+public class Controller {
+    private static Controller instance;
+
+    private Controller() {
+        Volo.inizializzaDatiTest();
+    }
+
+    public static Controller getInstance() {
+        if (instance == null) {
+            instance = new Controller();
+        }
+        return instance;
+    }
+
+    //gestione degli utenti
+    public boolean registraUtenteGenerico(String nome, String cognome, String username, String password) {
+        return UtenteGenerico.registraNuovo(nome, cognome, username, password);
+    }
+
+    public boolean registraAmministratore(String username, String password) {
+        return AmministratoreSistema.registraNuovo(username, password);
+    }
+
+    public Utente login(String username, String password) {
+        return Utente.autenticaUtente(username, password);
+    }
+
+    //gestione dei voli
+    public List<Volo> getAllVoli() {
+        return Volo.getTuttiVoli();
+    }
+
+    public List<VoloArrivo> getVoliArrivo() {
+        return Volo.getVoliArrivo();
+    }
+
+    public List<VoloPartenza> getVoliPartenza() {
+        return Volo.getVoliPartenza();
+    }
+
+    public void aggiungiVolo(Volo volo) {
+        Volo.aggiungiVolo(volo);
+    }
+
+    public Volo cercaVoloPerCodice(String codice) {
+        return Volo.cercaPerCodice(codice);
+    }
+
+    public VoloPartenza trovaVoloPartenza(String codice) {
+        return Volo.trovaVoloPartenza(codice);
+    }
+
+    //gestione delle prenotazioni
+    public Prenotazione creaPrenotazione(String nomePasseggero, String cognomePasseggero,
+                                         String codiceFiscale, Volo volo) {
+        return Prenotazione.creaPrenotazione(nomePasseggero, cognomePasseggero, codiceFiscale, volo);
+    }
+
+    public List<Prenotazione> cercaPrenotazioniPerPasseggero(String nome, String cognome) {
+        return Prenotazione.cercaPerPasseggero(nome, cognome);
+    }
+
+    public List<Prenotazione> cercaPrenotazioniPerCodiceVolo(String codiceVolo) {
+        return Prenotazione.cercaPerCodiceVolo(codiceVolo);
+    }
+}
